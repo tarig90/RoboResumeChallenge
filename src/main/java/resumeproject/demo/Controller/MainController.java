69343906b70a03ffd2423ecf0f1b9f33d27ad5fd
harambe143 <lava31@mail.com>
2017-08-04 20:43:24 -0400

    Commit  enters data into DB and display data from DB

--- src/main/java/resumeproject/demo/Controller/MainController.java
+++ src/main/java/resumeproject/demo/Controller/MainController.java
@@ -13,6 +13,13 @@ import resumeproject.demo.dalrepositories.Dalemprepository;
 import resumeproject.demo.modelzlayer.EmploymentData;
 
 import javax.validation.Valid;
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 
 @Controller
@@ -42,12 +49,53 @@ public class MainController
         return "addemployment";
     }
 
-    @PostMapping("/addemployment")
+   @PostMapping("/addemployment")
     public String postProduct( @ModelAttribute("newemp") EmploymentData emp)
     {
         dalemprepository.save(emp); // save it to the db
-        return "employeeconfirmation";
+       return "employeeconfirmation";
     }
+
+//
+//    @PostMapping("/enterinformation")
+//    public String postinfo(@Valid @ModelAttribute("newinfo") EmploymentData otherinfo, BindingResult bindingResult, Model newmodel) {
+//
+//        EmploymentData employeInfo =  new EmploymentData();
+//
+//        if (bindingResult.hasErrors())
+//        {
+//            return "addinfo";
+//        }
+//        System.out.println(otherinfo.getName());
+//
+//        try {
+//
+//            if(otherinfo.getEnddate()!=null)
+//            {
+//                otherinfo.setEndDate(df.format(date));
+//            }
+//
+//            long test;
+//            Date date1 = df.parse(otherinfo.getStartdate());
+//            Date date2 = df.parse(otherinfo.getEnddate());
+//
+//            long diff = date2.getTime() - date1.getTime();
+//            test = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+//            employeInfo.setDays(test);
+//            otherinfo.setDays(test);
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        newmodel.addAttribute("dates", employeInfo);
+//
+//        employeRepository.save(otherinfo);
+//
+//
+//        return "resultinfo";
+//    }
+
 
 
     @GetMapping("/displaydetails")
@@ -59,4 +107,8 @@ public class MainController
 
         return "displaydetails";
     }
+
+
+
+
 }
